74e2e1c2f73b7f143649ee19f9585b06bb8da5b2
Nicolas-Asafe <nicolasasafe45@gmail.com>
2025-06-27 12:00:03 -0300

    feat:[logs];fix:[economy]

--- core/entities/economy.go
+++ core/entities/economy.go
@@ -1,27 +1,35 @@
 package entities
 
+import "errors"
+
 type Economy struct {
-	MinValueForAdd           float64
-	MinValueForWithDrawMoney float64
-	MaxValueforWithDrawMoney float64
-	AuthorId                 string
-	Description              *string
-	Title                    string
-	Value                    float64
+	MinValueForAdd       float64
+	MinValueForWithdraw  float64
+	MaxValueForWithdraw  float64
+	AuthorID             string
+	Description          *string
+	Title                string
+	Value                float64
 }
 
-func (e *Economy) AddValueForValue(Value float64) {
-	if Value < e.MinValueForAdd {
+func (e *Economy) AddValue(amount float64) {
+	if amount < e.MinValueForAdd {
 		return
 	}
-	e.Value += Value
+	e.Value += amount
 }
 
-func (e *Economy) WithdrawMoney(Value float64) {
-	if Value > e.MaxValueforWithDrawMoney {
-		return
+func (e *Economy) Withdraw(amount float64) (float64, error) {
+	if amount > e.MaxValueForWithdraw {
+		return 0, errors.New("the maximum value was reached")
 	}
-	if Value < e.MinValueForWithDrawMoney {
-		return
+	if amount < e.MinValueForWithdraw {
+		return 0, errors.New("the minimum value was reached")
 	}
+	if amount > e.Value {
+		return 0, errors.New("you do not have enough balance")
+	}
+
+	e.Value -= amount
+	return amount, nil
 }
--- utils/logs.go
+++ utils/logs.go
@@ -1,1 +1,63 @@
 package utils
+
+import (
+	"fmt"
+	"time"
+)
+
+type Logs struct {
+	WithTime bool
+}
+
+func LogFormat(typeLog string, text string) {
+	fmt.Printf("[%s] %s", typeLog, text)
+}
+func LogFormatWithTime(typeLog string, text string, time string) {
+	fmt.Printf("[%s][%s] %s",time, typeLog, text)
+
+}
+
+func GetTime(l *Logs) string {
+	if l.WithTime {
+		time := time.Now().Format("2025-06-14 23:23:02")
+		return time
+	}
+	return ""
+}
+
+func (l *Logs) Info(text string) {
+	time := GetTime(l)
+	if time == "" {
+		LogFormat("INFO", text)
+		return
+	}
+	LogFormatWithTime("INFO",text,time)
+}
+
+func (l *Logs) Error(text string) {
+	time := GetTime(l)
+	if time == "" {
+		LogFormat("ERROR", text)
+		return
+	}
+	LogFormatWithTime("ERROR",text,time)
+}
+
+func (l *Logs) Warn(text string) {
+	time := GetTime(l)
+	if time == "" {
+		LogFormat("WARN", text)
+		return
+	}
+	LogFormatWithTime("WARN",text,time)
+}
+
+func (l *Logs) Debug(text string) {
+	time := GetTime(l)
+	if time == "" {
+		LogFormat("DEBUG", text)
+		return
+	}
+	LogFormatWithTime("DEBUG",text,time)
+}
+
