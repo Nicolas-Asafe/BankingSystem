6189b4fb3f54f1b0b8985afc6d04d06408dedd7d
Nicolas-Asafe <nicolasasafe45@gmail.com>
2025-06-28 13:03:40 -0300

    feat: ReciveFor[gin,log]

--- utils/responses.go
+++ utils/responses.go
@@ -1,11 +1,32 @@
 package utils
 
-type Response struct{
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Response struct {
 	Message string
-	Error error
-	Data *struct{}
+	Error   error
+	Data    *struct{}
 }
 
-func Resthis(Message string,Error error,Data *struct{})Response{
-	return Response{Message,Error,Data,}
+func Resthis(Message string, Error error, Data *struct{}) Response {
+	return Response{Message, Error, Data}
+}
+
+func ReciveForLog(response Response, logs *Logs) {
+	if response.Error != nil {
+		logs.Error(response.Error.Error())
+		return
+	}
+	logs.Info(response.Message)
+}
+
+func ReciveForGin(response Response, status int, ctx *gin.Context) {
+	ctx.JSON(status,
+		gin.H{
+			"message": response.Message,
+			"error":   response.Error.Error(),
+			"data":    response.Data,
+		})
 }
