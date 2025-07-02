71dc7d7f7bb6706c385c45c6e98623ce0fcaf554
Nicolas-Asafe <nicolasasafe45@gmail.com>
2025-07-01 23:08:41 -0300

    feat: newUser function in services users

--- core/services/userService.go
+++ core/services/userService.go
@@ -1,1 +1,20 @@
 package services
+
+import (
+	"github.com/Nicolas-Asafe/BankingSystem/core/entities"
+	"github.com/Nicolas-Asafe/BankingSystem/core/interfaces"
+	"github.com/Nicolas-Asafe/BankingSystem/utils"
+)
+
+type UserService struct{
+	Repo interfaces.UserRepository
+}
+
+
+func (u UserService) NewUser(User entities.User) utils.Response{
+	res:=u.Repo.NewUser(User)
+	if res.Error != nil {
+		utils.Resthis("Error for create user",res.Error,nil)
+	}
+	return utils.Resthis("User created successfully",nil,User)
+}
