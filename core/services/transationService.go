a24ca0519cd84467eb232fbc5d905fc3635a12ff
Nicolas-Asafe <nicolasasafe45@gmail.com>
2025-07-06 16:19:35 -0300

    feat: send transition in (Transition [service])

--- core/services/transationService.go
+++ core/services/transationService.go
@@ -1,1 +1,20 @@
 package services
+
+import (
+	"github.com/Nicolas-Asafe/BankingSystem/core/entities"
+	"github.com/Nicolas-Asafe/BankingSystem/core/interfaces"
+	"github.com/Nicolas-Asafe/BankingSystem/utils"
+)
+
+type TransitionService struct {
+	Repo interfaces.TransitionRepository
+}
+
+func (t TransitionService) SendTransition(tst entities.Transition){
+	res:=t.Repo.Send(tst)
+	if res.Error != nil{
+		utils.Resthis("Error performing transition",res.Error,nil)
+		return
+	}
+	utils.Resthis("Transaction completed successfully",nil,tst)
+}
