213998fcc5eca4c3a4c416209d11488b8332f728
Nicolas Asafe <nicolasasafe45@gmail.com>
2025-07-07 15:42:14 -0300

    fix: economy in entities and feat: add value & withdraw in economy repository

--- core/entities/economy.go
+++ core/entities/economy.go
@@ -16,32 +16,3 @@ type Economy struct {
 	Value               float64
 	Log                 utils.Logs
 }
-
-type EconomyReturn struct{
-	Can  bool
-	Value float64
-	Error error
-}
-
-func (e *Economy) AddValue(amount float64) EconomyReturn{
-	if amount < e.MinValueForAdd {
-		return EconomyReturn{false,0,errors.New("")}
-	}
-	valueFinal := e.Value+amount
-	return EconomyReturn{true,valueFinal,nil}
-}
-
-func (e *Economy) Withdraw(amount float64) EconomyReturn {
-	if amount > e.MaxValueForWithdraw {
-		return EconomyReturn{false,0, errors.New("the maximum value was reached")}
-	}
-	if amount < e.MinValueForWithdraw {
-		return EconomyReturn{false,0, errors.New("the maximum value was reached")}
-	}
-	if amount > e.Value {
-		return EconomyReturn{false,0,errors.New("you do not have enough balance")}
-	}
-
-	valueFinal:= e.Value-amount
-	return EconomyReturn{true,valueFinal,nil}
-}
--- core/interfaces/economyRepository.go
+++ core/interfaces/economyRepository.go
@@ -7,6 +7,8 @@ type EconomyRepository interface {
 	findOne(AuthoId string, EcoId string) utils.Response
 	find(AuthoId string) utils.Response
 	newEconomy(Economy entities.Economy) utils.Response
-	removeEconomy(Id int) utils.Response
+	removeEconomy(Id string) utils.Response
 	putName(newName string) utils.Response
+	AddValue(value float64,Id string) utils.Response
+	Withdraw(value float64,Id string) utils.Response
 }
